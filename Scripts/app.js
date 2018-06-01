52ead74bc8be27b351f0bb094efa93e0553fbc19
u04 <chowkas@rogers.com>
2018-06-01 19:44:52 -0400

    project page and other

--- Scripts/app.js
+++ Scripts/app.js
@@ -11,6 +11,12 @@
         let myTextAboutMe = "I am Dey. I don't want to write my full name here to make it harder for someone serching the web to link this page to me. Right now my mission is to make this webpage.";
         paragraphMe.textContent=myTextAboutMe;
 
+        
+        let titleMe= document.getElementById("meInMainPage");
+        let titleMeText = "me";
+        titleMe.textContent=titleMeText;
+    
+
     }
     window.addEventListener("load", DisplayText);
 
